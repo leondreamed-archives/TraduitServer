ef7f9d5c51ea63ce17708754ef21b4581ab53fc1
Leon Si <leonzalion@gmail.com>
2020-04-05 03:20:12 -0400

    made changes

--- controllers/users/notes/add.js
+++ controllers/users/notes/add.js
@@ -5,8 +5,8 @@ const Group = require('../../../models/Group');
 module.exports = async (req, res) => {
   let [userError, user] = await to(getUser(req));
   if (userError) return res.json(userError);
-  const {front, back} = req.body;
-  const [groupError] = await to(Group.findByIdAndUpdate(user.group, {$push: {notes: {front, back}}}).exec());
+  // req.body is an array of {front, back} notes
+  const [groupError] = await to(Group.findByIdAndUpdate(user.group, {$push: {notes: req.body}}).exec());
   if (groupError) return res.json(groupError);
   return res.json({success: true});
 };
