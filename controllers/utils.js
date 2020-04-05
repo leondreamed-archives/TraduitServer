809b64623455e24a634852132606b75c1c1825b2
Leon Si <leonzalion@gmail.com>
2020-04-04 23:19:54 -0400

    made changes

--- controllers/utils.js
+++ controllers/utils.js
@@ -5,7 +5,7 @@ const to = require('await-to-js').default;
 async function getUser(req) {
   const Authorization = req.get('Authorization');
   if (!Authorization) return Promise.reject({message: 'Not authorized'});
-  const [username, password] = Authorization.replace('Basic ', '').split(':');
+  let [username, password] = atob(Authorization.replace('Basic ', '')).split(':');
   const [userError, user] = await to(User.findOne({username})
   .select('+password').exec());
   if (userError) return Promise.reject(userError);
