310cfe652b54870f514682f5e4ea9efaebd1a2b8
Leon Si <leonzalion@gmail.com>
2020-04-05 00:54:24 -0400

    made changes

--- controllers/utils.js
+++ controllers/utils.js
@@ -10,7 +10,7 @@ async function getUser(req) {
   const [userError, user] = await to(User.findOne({username})
   .select('+password').exec());
   if (userError) return Promise.reject(userError);
-  console.log(user.password, password)
+  console.log(password, user.password)
   const match = await bcrypt.compare(user.password, password);
   if (!match) return Promise.reject({
     message: "Invalid username or password."
